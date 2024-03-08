64d459e0357520f0a538c75eb6a11792c735dabc
MarinTolic <marin.t91@gmail.com>
2024-03-08 17:36:00 +0100

    Initial commit

---
+++ server/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.ktor)
+    application
+}
+
+group = "hr.marintolic.chatsample"
+version = "1.0.0"
+application {
+    mainClass.set("hr.marintolic.chatsample.ApplicationKt")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["development"] ?: "false"}")
+}
+
+dependencies {
+    implementation(projects.shared)
+    implementation(libs.logback)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    testImplementation(libs.ktor.server.tests)
+    testImplementation(libs.kotlin.test.junit)
+}
